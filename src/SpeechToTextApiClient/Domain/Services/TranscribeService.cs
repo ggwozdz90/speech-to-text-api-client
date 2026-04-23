c44d0264fc2722214bac8478da1e2eb70bf93310
ggwozdz90 <40333300+ggwozdz90@users.noreply.github.com>
2026-04-23 23:29:51 +0200

    test

--- src/SpeechToTextApiClient/Domain/Services/TranscribeService.cs
+++ src/SpeechToTextApiClient/Domain/Services/TranscribeService.cs
@@ -17,61 +17,11 @@ internal sealed class TranscribeService(
 {
     public async Task<string> TranscribeAsync(string filePath, string sourceLanguage)
     {
-        logger.LogTrace(
-            "Transcribing file {FilePath} from {SourceLanguage} invoked from service...",
-            filePath,
-            sourceLanguage
-        );
 
-        try
-        {
-            return await speechToTextRepository.TranscribeAsync(filePath, sourceLanguage).ConfigureAwait(false);
-        }
-        catch (Exception ex) when (ex is NetworkException or TranscribeException or FileAccessException)
-        {
-            throw;
-        }
-        catch (Exception ex)
-        {
-            logger.LogError(
-                ex,
-                "An error occurred while transcribing the file {FilePath} from {SourceLanguage} from service...",
-                filePath,
-                sourceLanguage
-            );
-            throw;
-        }
     }
 
     public async Task<string> TranscribeAndTranslateAsync(string filePath, string sourceLanguage, string targetLanguage)
     {
-        logger.LogTrace(
-            "Transcribing and translating file {FilePath} from {SourceLanguage} to {TargetLanguage} invoked from service...",
-            filePath,
-            sourceLanguage,
-            targetLanguage
-        );
 
-        try
-        {
-            return await speechToTextRepository
-                .TranscribeAndTranslateAsync(filePath, sourceLanguage, targetLanguage)
-                .ConfigureAwait(false);
-        }
-        catch (Exception ex) when (ex is NetworkException or TranscribeException or FileAccessException)
-        {
-            throw;
-        }
-        catch (Exception ex)
-        {
-            logger.LogError(
-                ex,
-                "An error occurred while transcribing and translating the file {FilePath} from {SourceLanguage} to {TargetLanguage} from service...",
-                filePath,
-                sourceLanguage,
-                targetLanguage
-            );
-            throw;
-        }
     }
 }
